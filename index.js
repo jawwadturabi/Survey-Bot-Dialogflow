833fc0018fccd8d45ffdfa36d25c1ab4d05e0b18
jawwad-turabi-at-778284537906 <memonjawwad9@gmail.com>
2020-07-14 16:06:15 +0500

    initial commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) => {
 app.post('/webhook', (request, response) => {
 
     const _agent = new WebhookClient({ request, response });
-
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
     function welcome(agent) {
         agent.add("Hi! How are you doing?. Please tell me where do you live?")
         agent.add(new Suggestion(`Post-Code-1`));
@@ -19,12 +19,32 @@ app.post('/webhook', (request, response) => {
         agent.add(new Suggestion(`None of them`));
     }
 
+    function detail(agent) {
+        agent.add("What is your occupation?")
+        agent.add(new Suggestion(`Sedentary Occupation`));
+        agent.add(new Suggestion(`Standing Occupation`));
+        agent.add(new Suggestion(`Strenuous Occupation`));
+        agent.add(new Suggestion(`None of them`));
+    }
+
+    function more(agent) {
+        agent.add("What is your biological sex?")
+        agent.add(new Suggestion(`Male`));
+        agent.add(new Suggestion(`Female`));
+    }
+
+    function more(agent) {
+        agent.add("Do you shave everyday?")
+        agent.add(new Suggestion(`Yes`));
+        agent.add(new Suggestion(`No`));
+    }
+
 
     let intents = new Map();
     intents.set("Default Welcome Intent", welcome);
-    // intents.set("Start-quiz", startQuiz);
-    // intents.set("question", question)
-    // intents.set("question - yes", questionYes)
+    intents.set("details", detail);
+    intents.set("more", more)
+    intents.set("gender", gender)
     // intents.set("Bio", Bio)
     _agent.handleRequest(intents)
 });
