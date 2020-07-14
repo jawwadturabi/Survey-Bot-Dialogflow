5d85a7d3d7bb74bdc50bd0415ee60483c74e9e32
jawwad-turabi-at-778284537906 <memonjawwad9@gmail.com>
2020-07-14 15:51:53 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express().use(bodyParser.json());
+const { WebhookClient, Card, Suggestion } = require("dialogflow-fulfillment");
+
+app.post('/webhook', (request, response) => {
+
+    const _agent = new WebhookClient({ request, response });
+
+    function welcome(agent) {
+        agent.add("Hi! How are you doing?. Please tell me where do you live?")
+        agent.add(new Suggestion(`Post-Code-1`));
+        agent.add(new Suggestion(`Post-Code-2`));
+        agent.add(new Suggestion(`Post-Code-3`));
+        agent.add(new Suggestion(`None of them`));
+    }
+
+
+    let intents = new Map();
+    intents.set("Default Welcome Intent", welcome);
+    intents.set("Start-quiz", startQuiz);
+    intents.set("question", question)
+    intents.set("question - yes", questionYes)
+    intents.set("Bio", Bio)
+    _agent.handleRequest(intents)
+});
+
+app.listen(3000, () => {
+    console.log("server running on port 3000")
+})
